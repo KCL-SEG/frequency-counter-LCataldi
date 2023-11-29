360cde08b055ef4fa068a56b00ad20ddaecf5d18
LorenzoC <lorenzo.cataldi@icloud.com>
2023-11-29 18:07:35 +0000

    Check frequencies(items) function

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,14 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+    frequency_dict = {}
+
+    for item in items:
+
+        if str(item) not in frequency_dict.keys():
+            frequency_dict.update({str(item): 1})
+        else:
+            frequency_dict.update({str(item): frequency_dict[str(item)] + 1})
+
     return frequencies
